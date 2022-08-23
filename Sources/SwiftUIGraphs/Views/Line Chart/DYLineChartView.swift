b596f08983247aebe827d4191896b39f33998f51
Stefan Aleksiev <stefan.aleksiev@lab08.com>
2022-08-23 09:21:30 +0300

    [lineEnd] - add mask and remove trim

--- Sources/SwiftUIGraphs/Views/Line Chart/DYLineChartView.swift
+++ Sources/SwiftUIGraphs/Views/Line Chart/DYLineChartView.swift
@@ -277,10 +277,10 @@ public struct DYLineChartView: View, DYGridChart {
         Group {
             if self.dataPoints.count >= 2 {
                 self.pathFor(width: geo.size.width - marginSum, height: geo.size.height, closeShape: false)
-                    .trim(from: 0, to: self.settings.showAppearAnimation ? self.lineEnd == .zero ? 1 : self.lineEnd : 1)
                     .stroke(style: (self.settings as! DYLineChartSettings).lineStrokeStyle)
                     .foregroundColor((self.settings as! DYLineChartSettings).lineColor)
                     .shadow(color: (self.settings as! DYLineChartSettings).lineDropShadow?.color ?? .clear, radius:  (self.settings as! DYLineChartSettings).lineDropShadow?.radius ?? 0, x:  (self.settings as! DYLineChartSettings).lineDropShadow?.x ?? 0, y:  (self.settings as! DYLineChartSettings).lineDropShadow?.y ?? 0)
+                    .mask(lineAnimationMaskingView(width:geo.size.width))
             }
         }
       }
